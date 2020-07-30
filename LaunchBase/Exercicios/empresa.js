7f180ea5828de22c7566bfcd73adb301c096e068
Raphael Carneiro <raphaelgc.i9@gmail.com>
2020-07-29 23:14:35 -0300

    Added challenge 1.2(Lidando com objetos e vetores)

---
+++ LaunchBase/Exercicios/empresa.js
@@ -0,0 +1,8 @@
+const empresa = {
+      nome: "Rocketseat",
+      endereco: {
+          rua: "Rua Guilherme Gembala",
+          numero: "260" 
+      }
+}
+console.log(`A empresa ${empresa.nome} está localizada em ${empresa.endereco.rua}, ${empresa.endereco.numero}`);
