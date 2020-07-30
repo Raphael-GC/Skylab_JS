5aaf7e450de694a9b2061b3957eb97b843f59fe7
Raphael Carneiro <raphaelgc.i9@gmail.com>
2020-07-29 23:15:08 -0300

    Added challenge 1.2(Lidando com objetos e vetores)

---
+++ LaunchBase/Exercicios/programador.js
@@ -0,0 +1,11 @@
+const dev = {
+    nome: 'Raphael',
+    idade: '29',
+    techs: [
+        { nome: 'C++', especialidade: 'Desktop' },
+        { nome: 'Python', especialidade: 'Data Science' },
+        { nome: 'JavaScript', especialidade: 'Web/Mobile' } 
+    ]
+}
+
+console.log("O usuario "+dev.nome+" tem "+dev.idade+" anos e usa a tecnologia "+dev.techs[0].nome+" com especialidade em "+dev.techs[0].especialidade)
