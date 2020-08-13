f5608c725568b8010467e59b03c6bf5d7d9598b4
Raphael Carneiro <raphaelgc.i9@gmail.com>
2020-08-12 23:22:59 -0300

    Added challenge 1.3(Funções e estruturas de repetição)

---
+++ LaunchBase/Exercicios/percorrendo_array.js
@@ -0,0 +1,32 @@
+
+const usuarios = [
+    { nome: "Carlos", tecnologias: ["HTML", "CSS"] },
+    { nome: "Jasmine", tecnologias: ["JavaScript", "CSS"] },
+    { nome: "Tuane", tecnologias: ["HTML", "Node.js"] }
+]
+
+// Exercicio 1
+// for (let i = 0; i < usuarios.length; i++) {
+//     console.log(`${usuarios[i].nome} trabalha com ${usuarios[i].tecnologias[0]}, ${usuarios[i].tecnologias[1]}`); 
+// };
+
+// Exercicio 2
+// function cssUserCheck(techs) {
+//     let cssCheck = "";
+//     for (let i = 0; i < techs.length; i++) {
+//         if (techs[i] === "CSS"){
+//             cssCheck = true; 
+//         }
+//         else{ 
+//             cssCheck = false; 
+//         }   
+//     }return cssCheck;
+// }
+
+// for (let i = 0; i < usuarios.length; i++) {
+//     let cssUser = "";
+//     cssUser = cssUserCheck(usuarios[i].tecnologias);
+//     if (cssUser) {
+//         console.log(`O usuário ${usuarios[i].nome} trabalha com CSS`);
+//     }
+// };
