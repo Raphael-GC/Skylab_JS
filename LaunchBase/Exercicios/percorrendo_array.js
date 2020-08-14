c2e91a7224dd10b4c0de39c8bee87ff5ae815dba
Raphael Carneiro <raphaelgc.i9@gmail.com>
2020-08-13 23:56:31 -0300

    Updated Ex. 1-3 (Funções e estruturas de repetição)

--- LaunchBase/Exercicios/percorrendo_array.js
+++ LaunchBase/Exercicios/percorrendo_array.js
@@ -1,4 +1,3 @@
-
 const usuarios = [
     { nome: "Carlos", tecnologias: ["HTML", "CSS"] },
     { nome: "Jasmine", tecnologias: ["JavaScript", "CSS"] },
@@ -6,27 +5,27 @@ const usuarios = [
 ]
 
 // Exercicio 1
-// for (let i = 0; i < usuarios.length; i++) {
-//     console.log(`${usuarios[i].nome} trabalha com ${usuarios[i].tecnologias[0]}, ${usuarios[i].tecnologias[1]}`); 
-// };
+for (let i = 0; i < usuarios.length; i++) {
+    console.log(`${usuarios[i].nome} trabalha com ${usuarios[i].tecnologias[0]}, ${usuarios[i].tecnologias[1]}`); 
+};
 
 // Exercicio 2
-// function cssUserCheck(techs) {
-//     let cssCheck = "";
-//     for (let i = 0; i < techs.length; i++) {
-//         if (techs[i] === "CSS"){
-//             cssCheck = true; 
-//         }
-//         else{ 
-//             cssCheck = false; 
-//         }   
-//     }return cssCheck;
-// }
+function cssUserCheck(techs) {
+    let cssCheck = "";
+    for (let i = 0; i < techs.length; i++) {
+        if (techs[i] === "CSS"){
+            cssCheck = true; 
+        }
+        else{ 
+            cssCheck = false; 
+        }   
+    }return cssCheck;
+}
 
-// for (let i = 0; i < usuarios.length; i++) {
-//     let cssUser = "";
-//     cssUser = cssUserCheck(usuarios[i].tecnologias);
-//     if (cssUser) {
-//         console.log(`O usuário ${usuarios[i].nome} trabalha com CSS`);
-//     }
-// };
+for (let i = 0; i < usuarios.length; i++) {
+    let cssUser = "";
+    cssUser = cssUserCheck(usuarios[i].tecnologias);
+    if (cssUser) {
+        console.log(`O usuário ${usuarios[i].nome} trabalha com CSS`);
+    }
+};
